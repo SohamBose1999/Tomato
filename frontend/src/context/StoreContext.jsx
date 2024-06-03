7552b8d01415eff03a09078042d725fd9f72a800
Soham Bose <sohambose80@gmail.com>
2024-06-03 16:28:08 +0530

    css bug fixed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -34,8 +34,8 @@ const StoreContextProvider = (props) => {
         let totalAmount = 0;
         for (const item in cartItems) {
             if (cartItems[item] > 0) {
-                let itemInfo = food_list.find((product) => product._id === item);
-                totalAmount += itemInfo.price * cartItems[item];
+                let itemInfo = food_list?.find((product) => product._id === item);
+                totalAmount += itemInfo?.price * cartItems[item];
             }
         }
         return totalAmount;
