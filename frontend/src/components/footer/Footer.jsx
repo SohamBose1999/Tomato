612fd6a516fe9750674a6fabf8e852386f51c893
Soham Bose <sohambose80@gmail.com>
2024-06-06 16:41:55 +0530

    [changed the name of footer in frontend]-06/06/24

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
             </div>
            </div>
            <hr />
-            <p className="footer-copyright">Copyright 2024 @ Tomato.com - All Rights Reserved</p>
+            <p className="footer-copyright">Copyright 2024 @ sohambose80@gmail.com - All Rights Reserved</p>
 
     </div>
   )
