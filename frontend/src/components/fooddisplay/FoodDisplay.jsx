f5428b894109fc88104153a052f4911dd6bfaf22
Soham Bose <sohambose80@gmail.com>
2024-06-14 08:31:50 +0530

    [added loading animation for better UI]-14/06/24

--- frontend/src/components/fooddisplay/FoodDisplay.jsx
+++ frontend/src/components/fooddisplay/FoodDisplay.jsx
@@ -1,34 +1,42 @@
-import React, { useContext } from 'react'
-import "./FoodDisplay.css"
-import {StoreContext} from "../../context/StoreContext"
-import FoodItem from '../fooditem/FoodItem'
+
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../fooditem/FoodItem";
 import gif from "../../assets/delivery boy.gif"
 
-const FoodDisplay = ({category}) => {
-  const {food_list, loading}= useContext(StoreContext)
-
+const FoodDisplay = ({ category }) => {
+  const { food_list, loading } = useContext(StoreContext);
+  console.log(food_list);
   return (
-    <div className='food-display' id='food-display'>
-        <h2>Top Dishes Near You</h2>
-        {loading && (
-          <div className="gif-bike-parent">
-            <img className='gif-bike' src={gif} alt={'loading...'} />
-            <h4>Loading...</h4>
-          </div>
-        )}
-        {!loading && (
-        <div className="food-display-list">
-            {food_list.map((item,index)=>{
-              if(category==="All" || category===item.category){
-
-                return <FoodItem key={index} id={item._id} name={item.name} desc={item.description} price={item.price} image={item.image}/>
-              }
-                
-            })}
+    <div className="food-display" id="food-display">
+      <h2>Top Dishes Near You</h2>
+      {loading && (
+        <div className="gif-bike-parent">
+          <img className="gif-bike" src={gif} alt={'loading...'} />
+          <h4>Loading...</h4>
         </div>
-        )}
+      )}
+      {!loading && (
+        <div className="food-display-list">
+          {food_list.map((item, index) => {
+            if (category === "All" || category === item.category) {
+              return (
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  price={item.price}
+                  image={item.image}
+                  description={item.description}
+                />
+              );
+            }
+          })}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
--- frontend/src/pages/myorders/MyOrders.jsx
+++ frontend/src/pages/myorders/MyOrders.jsx
@@ -1,52 +1,94 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './MyOrders.css'
-import axios from 'axios'
-import { StoreContext } from '../../context/StoreContext';
-import { assets } from '../../assets/assets';
+import React, { useContext, useEffect, useState } from "react";
+import "./MyOrders.css";
+import axios from "axios";
+import { StoreContext } from "../../context/StoreContext";
+import { assets } from "../../assets/assets";
+import { jwtDecode } from "jwt-decode";
+import gif from "../../assets/delivery boy.gif"
 
 const MyOrders = () => {
-  
-  const [data,setData] =  useState([]);
-  const {url,token} = useContext(StoreContext);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { url, token } = useContext(StoreContext);
+
+  const decode = token ? jwtDecode(token) : null;
+
 
   const fetchOrders = async () => {
-    const response = await axios.post(url+"/api/order/userorders",{},{headers:{token}});
-    setData(response.data.data)
-  }
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        url + "/api/order/userorders",
+        {},
+        { headers: { token } }
+      );
+      setLoading(false);
+      setData(response.data.data);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     if (token) {
       fetchOrders();
     }
-  },[token])
+  }, [token]);
   return (
-    <div className='my-orders'>
-      <h2>My Orders</h2>
-      <div className="container">
-        {data?.reverse().map((order,index)=>{
-          return (
-            <div key={index} className='my-orders-order'>
-                <img src={assets.parcel_icon} alt="" />
-                <p>{order.items?.map((item,index)=>{
-                  if (index === order.items.length-1) {
-                    return item.name+" x "+item.quantity
-                  }
-                  else{
-                    return item.name+" x "+item.quantity+", "
-                  }
-                  
-                })}</p>
-                <p>₹{order.amount}.00</p>
-                <p>Items: {order.items.length}</p>
-                <p><span style={{fontSize:"16px",color: order.status === "Delivered"? 'green':'red'}}>&#x25cf;</span> <b>{order.status}</b></p>
-                <button onClick={()=>fetchOrders()}>Track Order</button>
-            </div>
-          )
-        })}
-      </div>
+    <div className="my-orders">
+      {loading ? <h2>Orders</h2> : <h2>{decode?.username}'s Orders</h2>}
+      {loading && (
+       <div className="gif-bike-parent">
+       <img className="gif-bike" src={gif} alt={'loading...'} />
+       <h4>Loading...</h4>
+     </div>
+      )}
+      {!loading && (
+        <div className="container">
+          {data?.length < 1 ? (
+            <p>No Orders Placed Yet !!</p>
+          ) : (
+            data?.reverse().map((order, index) => {
+              return (
+                <div key={index} className="my-orders-order">
+                  <img src={assets.parcel_icon} alt="" />
+                  <p>
+                    {order.items.map((item, index) => {
+                      if (index === order.items.length - 1) {
+                        return item.name + " x " + item.quantity;
+                      } else {
+                        return item.name + " x " + item.quantity + ", ";
+                      }
+                    })}
+                  </p>
+                  <p>₹{order.amount}.00</p>
+                  <p>Items: {order.items.length}</p>
+                  <p>
+                    <span
+                      style={{
+                        fontSize: "19px",
+                        color:
+                          order.status === "Delivered"
+                            ? "green"
+                            : order.status === "Out For Delivery"
+                            ? "orange"
+                            : "red",
+                      }}
+                    >
+                      &#x25cf;
+                    </span>{" "}
+                    <b>{order.status}</b>
+                  </p>
+                  <button onClick={() => fetchOrders()}>Track Order</button>
+                </div>
+              );
+            })
+          )}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MyOrders
-
+export default MyOrders;
